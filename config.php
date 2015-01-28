7d5bbcf6014d87ac0d29e704efa5d1c34518ef65
abuzer <abuzer.cs@gmail.com>
2015-01-29 00:43:01 +0500

    db, confiz

--- config.php
+++ config.php
@@ -8,20 +8,33 @@ define('MULTISITE', 0);
 /** MySQL database name for Osclass */
 define('DB_NAME', 'upetzqcs_bazar');
 
-/** MySQL database username */
-define('DB_USER', 'upetzqcs_zohaib');
+if ( $_SERVER['HTTP_HOST']  == 'kachehribazar.com'){
+	/** MySQL database username */
+	define('DB_USER', 'upetzqcs_zohaib');
 
-/** MySQL database password */
-define('DB_PASSWORD', 'imperial123');
+	/** MySQL database password */
+	define('DB_PASSWORD', 'imperial123');
+	
+	define('WEB_PATH', 'http://kachehribazar.com/');
 
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+}else{
+	/** MySQL database username */
+	define('DB_USER', 'root');
+
+	/** MySQL database password */
+	define('DB_PASSWORD', 'confiz');
+
+	define('WEB_PATH', 'http://localhost/kbazar/');
+}
+	/** MySQL hostname */
+	define('DB_HOST', 'localhost');
+
 
 /** Database Table prefix */
 define('DB_TABLE_PREFIX', 'z_');
 
 define('REL_WEB_URL', '/');
 
-define('WEB_PATH', 'http://kachehribazar.com/');
+
 
 ?>
